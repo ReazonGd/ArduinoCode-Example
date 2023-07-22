22beb36b9fa3126b426c1bfade1e2a8c38dc1fbb
Reazon <68423132+ReazonGd@users.noreply.github.com>
2023-07-22 11:06:12 +0700

    Update lampu dengan counter dan tombol reset.ino
    mengringkas

--- lampu lalu lintas/lampu dengan counter dan tombol reset.ino
+++ lampu lalu lintas/lampu dengan counter dan tombol reset.ino
@@ -1,4 +1,10 @@
-const byte shape[] = {
+/*
+ - lampu lalulintas dan hitung mundur + tombol reset -
+ Note: gini aj lu pusing? lemah.
+
+*/ 
+
+const byte digitPatterns[] = {
   B11111101,
   B01100001,
   B11011011,
@@ -12,77 +18,59 @@ const byte shape[] = {
   B00000001
 };
 
-const int segmenPins[] = { 5, 9, 8, 7, 6, 4, 3, 2 };
-const int buttonPins = 10; 
+       // 7segmen config: dp, g, f, e, d, c, b, a
+const int segmenPins[] = { 5, 9, 8, 7, 6, 4, 3, 2 },
+          lamp[] = { 12, 11, 10}, // trafic light
+          urutan[] = {9, 2, 7}, // 
+          buttonPins = 13;
 
-bool isPressed() {
-  if (digitalRead(buttonPins) == LOW) return true;
-  return false;
+int index = 0,
+    show = 0;
+
+void turnOn(int index) {
+  for(int pin : lamp){
+    digitalWrite(pin, pin == lamp[index]);
+  }
 }
-
-void showDigit(int number) {
-  Serial.print("showDigit :");
-  Serial.println(number);
-  boolean isBitSet;
-  for (int i = 11; i <= 9; i++) {
-    digitalWrite(i, LOW);
-  }
-  if (isPressed()) return run();
-  for (int i = 1; i < 8; i++) {
-    if (number < 0 || number > 9) isBitSet = 0;
-    else isBitSet = bitRead(shape[number], i);
-    isBitSet = !isBitSet;
-    digitalWrite(segmenPins[i], isBitSet);
-  }
-
-
-}
-
-void turnOn(int pins) {
-  for (int i = 11; i <= 13; i++) {
-    digitalWrite(i, LOW);
-  }
-  digitalWrite(pins, HIGH);
-}
-
-void run() {
-  turnOn(13);
-  for (int i = 9; i >= 1; i--) {
-    showDigit(i);
-    delay(1000);
-  }
-
-  turnOn(12);
-  for (int i = 2; i >= 1; i--) {
-    showDigit(i);
-    delay(1000);
-  }
-  turnOn(11);
-
-  for (int i = 8; i >= 1; i--) {
-    showDigit(i);
-    delay(1000);
-  }
-  run();
-}
-
-
 void setup() {
-  Serial.begin(9600);
-  for (int i = 0; i <= 9; i++) {
-    pinMode(segmenPins[i], OUTPUT);
+  for (int i = 2; i <= 12; i++) {
+    pinMode(i, OUTPUT);
   }
-
   pinMode(buttonPins, INPUT_PULLUP);
-
-
-  for (int i = 11; i <= 13; i++) {
-    pinMode(segmenPins[i], OUTPUT);
-  }
-  run();
+  turnOn(index);
 }
-
 
 void loop() {
-}
+  int show_number = urutan[index] - show;
 
+  // showing numbers in 7segmen 
+  for (int i = 1; i < 8; i++) {
+    // note: a bitRead reading bit from right to left,
+    digitalWrite(segmenPins[i], !bitRead(digitPatterns[show_number], i));
+  }
+  
+  // calculate next displaying numbers
+  show++;
+  if ((urutan[index] - show) < 0) {
+    show = 0;
+    index++;
+    if (index >= 3) index = 0;
+  }
+
+  // delay the number and cehking if reset button is pressed. 
+  int time = 0;
+  while (time < 990) {
+    if (digitalRead(buttonPins) == !1) {
+      index = 0;
+      show = 0;
+      time = 1000;
+      turnOn(index);
+    }
+    delay(10);
+    time += 10;
+  }
+
+
+  // refresh the trafic light when show number has reseted.
+  if (show == 0) turnOn(index);
+}
